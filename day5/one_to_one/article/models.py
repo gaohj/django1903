2fbeefaffd3e182ded39b4846d1278eb27771d20
zpc <zhao@163.com>
2019-12-06 12:03:26 +0800

    related_name

--- day5/one_to_one/article/models.py
+++ day5/one_to_one/article/models.py
@@ -6,12 +6,12 @@ class Category(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
-    # articles = models.ManyToManyField("Article")
+    articles = models.ManyToManyField("Article",related_name="tags")
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     category = models.ForeignKey('Category',on_delete=models.CASCADE,null=True,related_name="articles")
-    author = models.ForeignKey('frontuser.FrontUser',on_delete=models.CASCADE,null=True)
-    tags = models.ManyToManyField('Tag',related_name='tags')
+    author = models.ForeignKey('frontuser.FrontUser',on_delete=models.CASCADE,null=True,related_name="article",related_query_name='art')
+    # tags = models.ManyToManyField('Tag',related_name='tags')
     def __str__(self):
         return "Article:(title:%s,content:%s)" % (self.title,self.content)
--- day5/one_to_one/article/views.py
+++ day5/one_to_one/article/views.py
@@ -4,14 +4,20 @@ from .models import Article,Tag,Category
 from frontuser.models import FrontUser,UserExtension
 # Create your views here.
 def index(request):
-    user = FrontUser(username='鹏鹏')
-    user.save()
-    category = Category(name='日韩系列')
-    category.save()
-    article = Article(title='为什么我的眼睛如此亮',content='因为眼睛里全是你 你的名字')
-    article.category = category
-    article.author = user
-    article.save()
+    # user = FrontUser(username='鹏鹏')
+    # user.save()
+    # category = Category(name='日韩系列')
+    # category.save()
+    # article = Article(title='为什么我的眼睛如此亮',content='因为眼睛里全是你 你的名字')
+    # article.category = category
+    # article.author = user
+    # article.save()
+    #查询发表了我要在你身上做春天对樱桃做的事情 这篇文章的作者信息
+    #如果加了 related_query_name='art'
+    # user = FrontUser.objects.filter(art__title='我要在你身上做春天对樱桃做的事情')
+    # print(user)
+    user = FrontUser.objects.get(pk=3)
+    print(user.article.all()) #这里不能用 art 还是只能用article
     return HttpResponse('首页')
 
 def one_to_one_view(request):
@@ -30,11 +36,20 @@ def one_to_one_view(request):
     return HttpResponse('one_to_one_view success')
 
 def many_to_many_view(request):
-    article = Article.objects.get(pk=2)
-    tag = Tag(name='国产系列')
-    tag.save()
+    # article = Article.objects.get(pk=2)
+    # tag = Tag.objects.get(pk=5)
     # tag.articles.add(article)
     # article.tag_set.add(tag)
     # tag.article_set.add(article)
-    article.tags.add(tag)
+    # article.tags.add(tag)
+    # tag = Tag.objects.get(pk=4)
+    # articles = tag.tags.all()
+    # print(articles)
+    # for article in  articles:
+    #     print(article)
+    article = Article.objects.get(pk=2)
+    tags = article.tags.all()
+    print(tags)
+    for tag in tags:
+        print(tag)
     return HttpResponse('many_to_many_view success')
